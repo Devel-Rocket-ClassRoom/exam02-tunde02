0ecde27bd97fb630aaa29dd0f6bebc5cb1a31112
Seongho Jin <snskrktja@naver.com>
2026-06-12 14:01:09 +0900

    chore: 이전 기획에 있던 내용 삭제
    - Gravity, Ground 등 더이상 필요 없는 내용 삭제

--- Unreal10th_ConsoleGameProject/Wall.cpp
+++ Unreal10th_ConsoleGameProject/Wall.cpp
@@ -15,11 +15,6 @@ Wall::Wall(int InX, int InY, size_t InWidth, size_t InHeight)
     Transform_.Height = InHeight;
     NextPosition_ = Transform_.Position;
 
-    Collider_.Initialize(Transform_);
-    CollisionLayer_ = CollisionLayer::Wall;
-
-    bUseGravity_ = false;
-
     RenderString_.reserve(Transform_.Width * Transform_.Height);
     for (int i = 0; i < Transform_.Height; i++)
     {
